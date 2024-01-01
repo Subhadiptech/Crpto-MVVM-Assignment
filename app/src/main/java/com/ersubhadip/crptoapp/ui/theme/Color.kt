77bcb7205a5878e0b67b2d5490ff79267a17ee63
Subhadiptech <subhadipdhn@gmail.com>
2024-01-01 21:02:27 +0530

    basic theme setup done

--- app/src/main/java/com/ersubhadip/crptoapp/ui/theme/Color.kt
+++ app/src/main/java/com/ersubhadip/crptoapp/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.ersubhadip.crptoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBackground = Color(0xff1a212d)
+val White = Color(0xffffffff)
+val PrimaryBlue = Color(0xff3083ce)
+val PrimaryPurple = Color(0xffb692f5)
+val SlateDark = Color(0xff2c3749)
+val Green = Color(0xff30855b)
--- app/src/main/java/com/ersubhadip/crptoapp/ui/theme/Type.kt
+++ app/src/main/java/com/ersubhadip/crptoapp/ui/theme/Type.kt
@@ -2,14 +2,16 @@ package com.ersubhadip.crptoapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ersubhadip.crptoapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.lexend_deca)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
